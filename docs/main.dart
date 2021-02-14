3d826957b818b1374fce1500da1bf10bc1f90bfe
HALKA9000-NIIP <csta19097@g.nihon-u.ac.jp>
2021-02-14 16:06:43 +0900

    ud: kawaii interface 0.0.1

--- docs/main.dart
+++ docs/main.dart
@@ -1,7 +1,18 @@
 import "dart:html";
 
 void main() {
-  String textBody = "";
+  String kai = (querySelector('#usernames') as InputElement)?.value;
+  String textBody = "test";
+  querySelectorAll('input[name=tweet-type]').onClick.listen(clickHandler);
+}
+void clickHandler(MouseEvent e){
+  String type = (querySelector('input[name=tweet-type]:checked') as InputElement)?.value;
+  textView(type);
+  
+}
+void textView(String text){
   String thisURL ="https://developerspackages-org.github.io/KawaiiTweetGenerator/";
-  String intentURL = "https://twitter.com/intent/tweet?text=${textBody}&url=${thisURL}&related=DevPackOfficial";
+  String intentURL = "https://twitter.com/intent/tweet?text=${text}&url=${thisURL}&related=DevPackOfficial";
+  querySelector('#main-text')?.text = text;
+  querySelector('#tweet')?.setAttribute("href",intentURL);
 }
